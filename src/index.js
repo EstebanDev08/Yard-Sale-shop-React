47011ef8c2128bc273b76647807c74676a1d3179
Esteban <elmejormaxsimus320@gmail.com>
2022-09-24 02:19:48 -0500

    base del proyecto listas

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import App from "./componets/App";
+
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <App />, document.getElementById('app')
+)
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtract = require('mini-css-extract-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+    },
+
+    mode: 'development',
+
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node__modules/,
+                use: [
+                    {
+                        loader: 'babel-loader'
+                    }
+
+                ]
+
+
+
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader'
+                    },
+                ],
+
+
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+
+                ],
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+
+        new MiniCssExtract({
+            filename: '[name].css'
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 3005,
+    }
+}
